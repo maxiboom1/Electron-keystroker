4b563e7467da8e8a1de72c58451c25e4b7b9b7f5
Alex Samih-Zade <galutziogen@gmail.com>
2024-03-12 12:54:35 +0200

    ..

--- src/main/main.js
+++ src/main/main.js
@@ -7,7 +7,7 @@ let tray = null;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 300,
     resizable: false, // Set resizable to false
     webPreferences: {
